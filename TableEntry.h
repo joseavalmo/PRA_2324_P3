5a65b387a8fd1c5d7c254528bbaf1d9f92d7a4ac
joseavalmo <javalmo1@edu.upv.es>
2024-01-13 20:25:33 +0100

    Añadida implementación de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -1,41 +1,54 @@
 #ifndef TABLEENTRY_H
 #define TABLEENTRY_H
-
 #include <string>
 #include <ostream>
+using namespace std;
 
-template <typename V> 
-class TableEntry {
-    public:
-    std::string key;
-    V value;
+template <typename V> class TableEntry{
 
-    TableEntry(std::string key, V value){
-        this->key = key;
-        this->value = value;
-    }
-        
-    TableEntry(std::string key){
-        this->key = key;
-    }
+	public:
+		string key;
+		V value;
 
-    TableEntry(){
-        this->key = "";
-    }
+		TableEntry(string key, V value){
+			this->key = key;
+			this->value = value;
+		}
 
-    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-        return te1.key == te2.key;
-    }
+		TableEntry(string key){
+			this->key = key;
+		}
 
-    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
-        return !(te1 == te2);
-    }
+		TableEntry(){
+			this->key = "";
+		}
 
-    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-        out <<"('" << te.key <<"' => " << te.value << ")";
-        return out;
-    }
-    
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key == te2.key)return true;
+			else return false;
+		}
+
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key != te2.key) return true;
+			else return false;
+		}
+
+		friend ostream&operator<<(ostream &out, const TableEntry<V> &te){
+			out << "\t(" << te.key << "' => " << te.value << ")\n";
+			return out;
+		}
+		
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key < te2.key) return true;
+			else return false;
+		}
+		
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key > te2.key) return true;
+			else return false;
+		}
+
+
 };
 
 #endif
